eb945724519e7ea1605acb1651824e979b9bfdfc
HEL70N <heltonrodolfolourenco.01@gmail.com>
2023-07-24 18:54:35 +0200

    Processando Cadastro

--- src/Controller/MyexpensesController.php
+++ src/Controller/MyexpensesController.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace Code\Controller;
 
+use Code\DB\Connection;
+use Code\Entity\Expense;
 use Code\View\View;
 
 class MyExpensesController
@@ -8,11 +11,22 @@ class MyExpensesController
 
     public function index()
     {
-        print 'Isso é o Index';
+        var_dump((new Expense(Connection::getInstance()))->findAll());
     }
 
     public function new()
     {
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method == 'POST') {
+            $data = $_POST;
+            
+            $expense = new Expense(Connection::getInstance());
+            $expense->insert($data);
+
+            return header('Location: ' . HOME . '/myexpenses');
+        }
+
         $view = new View('expenses/new.phtml');
         return $view->render();
     }
