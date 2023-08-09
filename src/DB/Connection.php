bd1ae0f903bb55dcc4fca1f54549954d631fd0a0
HEL70N <heltonrodolfolourenco.01@gmail.com>
2023-08-09 18:05:27 +0200

    Lançando Exceptions no PDO

--- src/DB/Connection.php
+++ src/DB/Connection.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Code\DB;
+
 class Connection
 {
     private static $instance = null;
@@ -11,7 +13,7 @@ class Connection
     {
         if (is_null(self::$instance)) {
             self::$instance = new \PDO('mysql:host=127.0.0.1;dbname=my_expenses', 'root', '');
-
+            self::$instance->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             self::$instance->exec('SET NAMES UTF8');
         }
 
