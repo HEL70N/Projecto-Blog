0c195d8949c08b3021a0bf0df32b4e4c72a1240b
HEL70N <heltonrodolfolourenco.01@gmail.com>
2023-08-10 18:40:18 +0200

    Concluido Cadastro de Gastos

--- src/Controller/MyexpensesController.php
+++ src/Controller/MyexpensesController.php
@@ -3,7 +3,9 @@
 namespace Code\Controller;
 
 use Code\DB\Connection;
+use Code\Entity\Category;
 use Code\Entity\Expense;
+use Code\Entity\User;
 use Code\View\View;
 
 class MyExpensesController
@@ -17,17 +19,22 @@ class MyExpensesController
     public function new()
     {
         $method = $_SERVER['REQUEST_METHOD'];
+        $connection = Connection::getInstance();
 
         if ($method == 'POST') {
             $data = $_POST;
-            
-            $expense = new Expense(Connection::getInstance());
+
+            $expense = new Expense($connection);
             $expense->insert($data);
 
             return header('Location: ' . HOME . '/myexpenses');
         }
 
         $view = new View('expenses/new.phtml');
+
+        $view->categories = (new Category($connection))->findAll();
+        $view->users = (new User($connection))->findAll();
+
         return $view->render();
     }
 }
