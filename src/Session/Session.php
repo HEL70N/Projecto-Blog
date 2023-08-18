98e5ab7a86efba88f7898232c3d90b91362108db
HEL70N <heltonrodolfolourenco.01@gmail.com>
2023-08-18 16:03:07 +0200

    Iniciando Authenticator

--- src/Session/Session.php
+++ src/Session/Session.php
@@ -14,13 +14,21 @@ class Session
 
     public static function add($key, $value)
     {
+        self::sessionStart();
         $_SESSION[$key] = $value;
     }
 
     public static function remove($key)
     {
+        self::sessionStart();
         if (isset($_SESSION[$key])) {
             unset($_SESSION[$key]);
         }
+    }
+
+    public static function has($key)
+    {
+        self::sessionStart();
+        return isset($_SESSION[$key]);
     }
 }
